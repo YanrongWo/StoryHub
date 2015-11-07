7206359729ecd1140ff5aada5f39502ad3477393
Rona (Yanrong Wo) <yw2513@columbia.edu>
2015-11-06 22:07:10 -0500

    Passed facebook name to backend

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,12 +2,24 @@ package controllers;
 import play.*;
 import play.mvc.*;
 import models.*;
-
+import play.data.DynamicForm;
+import play.data.Form;
 import views.html.*;
+
 
 public class Application extends Controller {
 
     public Result index() {
         return ok(index.render("ASDFASFASDF World."));
+    }
+
+    public Result facebookName() {
+    	DynamicForm form = Form.form().bindFromRequest();
+    	if (form.data().size() != 0)
+    	{
+    		System.out.println(form.get("name"));
+    		return ok("Got name!");
+    	}
+    	return badRequest();
     }
 }
