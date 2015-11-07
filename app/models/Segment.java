f0173459ace94b1a9de8f289117977ff65062036
btwang05 <btwang05@gmail.com>
2015-11-06 20:00:32 -0500

    Added accessor methods to Segment

--- app/models/Segment.java
+++ app/models/Segment.java
@@ -13,12 +13,50 @@ public class Segment{
 	private Segment parentSeg;
 	private ArrayList<Segment> childSegs;
 	
-	public Segment(Segment parent) {
-		this.parentSeg = parent;
+	public Segment(Segment parentSeg, String title, String author, StringBuffer content, int id, ArrayList<String> tags) {
+		this.parentSeg = parentSeg;
+        this.title = title;
+        this.author = author;
+        this.content = content;
+        this.id = id;
+        this.tags = tags;
+        this.childSegs = new ArrayList<Segment>();
+        
 	}
 	
 	public boolean addChild(Segment child) {
 		return childSegs.add(child);
 	}
 	
+    public Segment getParent() {
+        return this.parentSeg;
+    }
+    
+    public String getTitle() {
+        return this.title;
+    }
+    
+    public String getAuthor() {
+        return this.author;
+    }
+    
+    public StringBuffer getContent() {
+        return this.content;
+    }
+    
+    public int getSegmentId() {
+        return this.id;
+    }
+    
+    public ArrayList<String> getTags() {
+        return this.tags;
+    }
+    
+    public Segment getParentSeg() {
+        return this.parentSeg;
+    }
+    
+    public ArrayList<Segment> getChildSegs() {
+        return this.childSegs;
+    }
 }
