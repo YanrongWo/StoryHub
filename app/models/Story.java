8d4b900a62931d2fd8656ad60b8a450f5d28f257
btwang05 <btwang05@gmail.com>
2015-11-15 16:00:03 -0500

    Commented story and removed extraneous functions

--- app/models/Story.java
+++ app/models/Story.java
@@ -3,23 +3,36 @@ import java.util.*;
 import javax.persistence.*;
 import java.io.*;
 
+/**
+ * Story represents one story, with Segments in a tree structure.
+ * @author      Java the Hutt
+ * @version     0.1
+ */
 public class Story implements Serializable{
+
+	//story id
 	private int id;
 
 	private Segment root;
 	private int nextSegId = 0;
+
+	//serialVersionUID used for serialization
 	private static final long serialVersionUID = 1L;
 
 	public Story(Segment aRoot, int aId){
 		this.id = aId;
-		
 		aRoot.setStoryId(this.id);
 		aRoot.setSegmentId(this.nextSegId);
 		this.root = aRoot;
 		this.nextSegId++;
 	}
 
-	//returns the segment with segId
+   /**
+	* Helper function to recursively search for segment matching segId
+	* @param Segment id used to search
+	* @param Segment to search with
+	* @return Segment found that matched segId
+	*/
 	private Segment recursiveSearchSegId(int segId, Segment seg){
 		if (seg.getSegmentId()==segId)
 		    return seg;
@@ -31,39 +44,34 @@ public class Story implements Serializable{
 		return result;
 	}
 
+   /**
+	* Function to search for segment matching segId.
+	* Uses helper recursiveSearchSegId function
+	* @param Segment id used to search
+	* @return Segment found that matched segId else null
+	*/
 	public Segment findSegById(int segId) {
 		return this.recursiveSearchSegId(segId, this.root);
 	}
 
-	public boolean hasTag(String aTag){
-		return recurseHasTag(aTag, this.root);
-	}
-
-	public boolean recurseHasTag(String aTag, Segment seg) {
-		if (seg.hasTag(aTag)) {
-			return true;
-		} else {
-			ArrayList<Segment> children = seg.getChildSegs();
-			for (int i = 0; i < children.size(); i++) {         
-		    	recurseHasTag(aTag, children.get(i));
-			}
-		}
-		return false;
-	}
-
-	// Returns list of segment ids of leaf nodes that are matching the tag
+   /**
+	* Function to return an arraylist of segments with the searched tag
+	* Uses helper recursiveSearchSegTag
+	* @param String tag to search
+	* @return ArrayList of Segment that match the tag
+	*/
 	public ArrayList<Segment> findTags(String searchWord){
 		ArrayList<Segment> segments = new ArrayList<Segment>();
 		recurseSearchSegTag(segments, this.root, searchWord);
 		return segments;
 	}
 
-	public ArrayList <Segment> findTitles(String searchWord){
-		ArrayList<Segment> segments = new ArrayList<Segment>();
-		recurseSearchSegTitle(segments,this.root,searchWord);
-		return segments;
-	}
-
+   /**
+	* Helper function to recursively search for segment matching tag
+	* @param ArrayList of segments that have already been matched with tag
+	* @param Segment to search with
+	* @param tag used to search
+	*/
 	private void recurseSearchSegTag(ArrayList<Segment> segments, Segment seg, String searchWord) {
 		for (String tag : seg.getTags()){
 			if(tag.toLowerCase().equals(searchWord.toLowerCase())){
@@ -81,7 +89,25 @@ public class Story implements Serializable{
 		}
 	}
 
+   /**
+	* Function to return an arraylist of segments with the searched title keyword
+	* Uses helper recursiveSearchSegTitles
+	* @param String keyword to search in title
+	* @return ArrayList of Segment that match the title keyword
+	*/
+	public ArrayList <Segment> findTitles(String searchWord){
+		ArrayList<Segment> segments = new ArrayList<Segment>();
+		recurseSearchSegTitle(segments,this.root,searchWord);
+		return segments;
+	}
 
+
+   /**
+	* Helper function to recursively search for segment matching title keyword
+	* @param ArrayList of segments that have already been matched with title keyword
+	* @param Segment to search with
+	* @param title keyword used to search
+	*/
 	private void recurseSearchSegTitle(ArrayList<Segment> segments, Segment seg, String searchWord){
 		if(seg.getTitle().toLowerCase().contains(searchWord.toLowerCase())){
 			segments.add(seg);
@@ -97,24 +123,7 @@ public class Story implements Serializable{
 		}
 	}
 
-
-	//returns arraylist of parents of the given segment seg
-	public ArrayList<Segment> getParents(Segment seg) {
-		ArrayList<Segment> parents = new ArrayList<Segment>();
-		recursiveParents(parents, seg);
-		return parents;
-	}
-
-	private void recursiveParents(ArrayList<Segment> parents, Segment seg)
-	{
-		Segment parent = seg.getParentSeg();
-		if (parent != null) {
-			parents.add(parent);
-			recursiveParents(parents, parent);
-		} else {
-			return;
-		}
-	}
+	//accessors and mutators
 
 	public int setStoryId(int aId){
 		this.id = aId;
@@ -129,12 +138,12 @@ public class Story implements Serializable{
 		return this.root;
 	}
 
-	public boolean fork(Segment seg, int segId) {
-		Segment segToFork = this.findSegById(segId);
-		return addSegment(seg, segToFork.getParentSeg().getSegmentId());
-	}
-
-	// Adds Segment seg as a child to Segment with segId
+   /**
+	* Adds a segment seg as a child of the segment with segId
+	* @param Segment to be added
+	* @param int segId of the segment to be added to
+	* @return true if add child was successful
+	*/
 	public boolean addSegment(Segment seg, int segId){
 		Segment segToFork = this.findSegById(segId);
 		if(segToFork != null){
