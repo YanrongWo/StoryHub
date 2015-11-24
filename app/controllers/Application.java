8158c29eb6c07031df3e20865b518dfe185ac06e
Kellie Ren Lu <krl2130@columbia.edu>
2015-11-24 17:32:02 -0500

    Added try catch for Application functions

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -17,28 +17,36 @@ public class Application extends Controller {
     /* Handles GET requests from / - home page 
      * Displays the home page stories with no offset
      */
-    public Result index() throws SQLException, IOException, ClassNotFoundException{
+    public Result index() {
         //Get all stories with offset 0
-        ArrayList<Story> storyList = myAppController.getFrontPageStories(0);
-        int interval = myAppController.getMax();
-        int storySize = myAppController.getStoryListSize();  
-        return ok(index.render("Homepage", storyList, 0, storySize, interval));
+        try{
+            ArrayList<Story> storyList = myAppController.getFrontPageStories(0);
+            int interval = myAppController.getMax();
+            int storySize = myAppController.getStoryListSize();  
+            return ok(index.render("Homepage", storyList, 0, storySize, interval));
+        } catch ( SQLException e | IOException i | ClassNotFoundException c){
+            return internalServerError(views.html.error.render("Something went awfully wrong...please contant the website administrator."));
+        }
     }
 
     /* Handles GET requests from /offset - home page with offset 
      * Displays the home page stories with offset
      * @param i - offset of the home page
      */
-    public Result offset(int i) throws SQLException, IOException, ClassNotFoundException{
-        int storySize = myAppController.getStoryListSize();
-        // Invalid offset
-        if(i > storySize) {
-            String message = "Your offset is larger than the size of the stories library: " + Integer.toString(storySize);
-            return notFound(views.html.error.render("Page Not Found"));
+    public Result offset(int i) {
+        try{
+            int storySize = myAppController.getStoryListSize();
+            // Invalid offset
+            if(i > storySize) {
+                String message = "Your offset is larger than the size of the stories library: " + Integer.toString(storySize);
+                return notFound(views.html.error.render("Page Not Found"));
+            }
+            //Get all stories with offset i
+            ArrayList<Story> storyList = myAppController.getFrontPageStories(i);   
+            return ok(index.render("Homepage", storyList, i, storySize, myAppController.getMax()));
+        } catch ( SQLException e | IOException i | ClassNotFoundException c){
+            return internalServerError(views.html.error.render("Something went awfully wrong...please contant the website administrator."));
         }
-        //Get all stories with offset i
-        ArrayList<Story> storyList = myAppController.getFrontPageStories(i);   
-        return ok(index.render("Homepage", storyList, i, storySize, myAppController.getMax()));
     }
 
     /* Handles POST requests from /FacebookName - facebook login 
@@ -91,46 +99,50 @@ public class Application extends Controller {
      * and cleans up input for storage
      * Returns the new story id and the root id to the front end
      */
-    public Result newStorySubmit() throws SQLException, IOException, ClassNotFoundException{
+    public Result newStorySubmit(){
         //Check logged in
-        if (session("name") != null){
-            DynamicForm form = Form.form().bindFromRequest();
-            if (form.data().size() == 0) {
-                return badRequest("Form Error");
-            } else {
-                String title = form.get("title").replaceAll("\"", "\'");
-                String content = form.get("content").replaceAll("\"", "\'");
-                //Checks that the story doesn't have the same content as 
-                //another root
-                myAppController.loadAll();
-                ArrayList<Story> allStories = myAppController.getStories();
-                for (int i = 0; i < allStories.size(); i++){
-                    if (allStories.get(i).getRoot().getContent().equals(content)){
-                        int id = allStories.get(i).getStoryId();
-                        String message = " <a href=\"/Story/" + id 
-                        + "/0\"> Error! A story with the same content has already been made! </a>";
-                        return ok("not found");
+        try{
+            if (session("name") != null){
+                DynamicForm form = Form.form().bindFromRequest();
+                if (form.data().size() == 0) {
+                    return badRequest("Form Error");
+                } else {
+                    String title = form.get("title").replaceAll("\"", "\'");
+                    String content = form.get("content").replaceAll("\"", "\'");
+                    //Checks that the story doesn't have the same content as 
+                    //another root
+                    myAppController.loadAll();
+                    ArrayList<Story> allStories = myAppController.getStories();
+                    for (int i = 0; i < allStories.size(); i++){
+                        if (allStories.get(i).getRoot().getContent().equals(content)){
+                            int id = allStories.get(i).getStoryId();
+                            String message = " <a href=\"/Story/" + id 
+                            + "/0\"> Error! A story with the same content has already been made! </a>";
+                            return ok("not found");
+                        }
                     }
+                    //Parse and make sure the tags aren't repeats
+                    String tagsRaw = form.get("tags").replaceAll("\"", "\'");
+                    String[] tags = tagsRaw.replaceAll("#", "").split(" ");
+                    Set<String> setTags = new HashSet<String>(Arrays.asList(tags));
+                    setTags.remove("");
+                    setTags.remove(" ");
+                    String[] uniqueTags = setTags.toArray(new String[setTags.size()]);
+
+                    //Create root and then the story
+                    Segment seg = new Segment(null, title, session("name"),
+                        content, uniqueTags);
+                    Story myStory = myAppController.createStory(seg);
+
+                    //Create result with story id, root id
+                    String result = Integer.toString(myStory.getStoryId()) + "," + Integer.toString(0);
+                    return ok(result);
                 }
-                //Parse and make sure the tags aren't repeats
-                String tagsRaw = form.get("tags").replaceAll("\"", "\'");
-                String[] tags = tagsRaw.replaceAll("#", "").split(" ");
-                Set<String> setTags = new HashSet<String>(Arrays.asList(tags));
-                setTags.remove("");
-                setTags.remove(" ");
-                String[] uniqueTags = setTags.toArray(new String[setTags.size()]);
-
-                //Create root and then the story
-                Segment seg = new Segment(null, title, session("name"),
-                    content, uniqueTags);
-                Story myStory = myAppController.createStory(seg);
-
-                //Create result with story id, root id
-                String result = Integer.toString(myStory.getStoryId()) + "," + Integer.toString(0);
-                return ok(result);
             }
+            return badRequest(error.render("You must be logged in to add a story"));
+        } catch ( SQLException e | IOException i | ClassNotFoundException c){
+            return internalServerError(views.html.error.render("Something went awfully wrong...please contant the website administrator."));
         }
-        return badRequest(error.render("You must be logged in to add a story"));
     }
 
     /* Handles GET request from: /Error/ERROR 
@@ -144,18 +156,22 @@ public class Application extends Controller {
         @param storyId - story ID that you want to add a new segment too
         @param segmentId - the segment that you are branching off to add a new segment to
         Displays a page to add a new segment */
-        public Result newFork(int storyId, int segmentId) throws SQLException, IOException, ClassNotFoundException{
-            Story myStory = myAppController.getStory(storyId);
-            if (session("name") != null)
-            {
-                if(!myStory.isClosed()){
-                    return  ok(newStory.render("New Segment", "newFork"));
-                } else{
-                    return badRequest(error.render("This story has been closed"));
+        public Result newFork(int storyId, int segmentId){
+            try{
+                Story myStory = myAppController.getStory(storyId);
+                if (session("name") != null)
+                {
+                    if(!myStory.isClosed()){
+                        return  ok(newStory.render("New Segment", "newFork"));
+                    } else{
+                        return badRequest(error.render("This story has been closed"));
+                    }
+                    
                 }
-                
+                return badRequest(error.render("You must be logged in to add a segment"));
+            } catch ( SQLException e | IOException i | ClassNotFoundException c){
+            return internalServerError(views.html.error.render("Something went awfully wrong...please contant the website administrator."));
             }
-            return badRequest(error.render("You must be logged in to add a segment"));
         }
 
     /* Handles POST request from: /Story/STORYID/SEGMENTID/NewSegment 
@@ -208,61 +224,73 @@ public class Application extends Controller {
         @ param id - story ID of the story the segment is in
         @ param segid - segment ID of the segment of interest
         Returns a page listing the story at the specified segment id  */
-        public Result story(int id, int segid)throws SQLException, IOException, ClassNotFoundException{
-            boolean loggedIn = (session("name") != null);
-            Story myStory = myAppController.getStory(id);
-            if (myStory != null){
-                Segment mySeg = myStory.findSegById(segid);
-                if (mySeg == null){
-                    return notFound(views.html.error.render("Page Not Found"));
+        public Result story(int id, int segid){
+            try{
+                boolean loggedIn = (session("name") != null);
+                Story myStory = myAppController.getStory(id);
+                if (myStory != null){
+                    Segment mySeg = myStory.findSegById(segid);
+                    if (mySeg == null){
+                        return notFound(views.html.error.render("Page Not Found"));
+                    }
+                    ArrayList<Integer> segsToParent = mySeg.getParentSegIds();
+                    return ok(story.render(id, segsToParent, loggedIn, myStory.isClosed()));
                 }
-                ArrayList<Integer> segsToParent = mySeg.getParentSegIds();
-                return ok(story.render(id, segsToParent, loggedIn, myStory.isClosed()));
+                return notFound(views.html.error.render("Page Not Found"));
+            } catch ( SQLException e | IOException i | ClassNotFoundException c){
+                return internalServerError(views.html.error.render("Something went awfully wrong...please contant the website administrator."));
             }
-            return notFound(views.html.error.render("Page Not Found"));
         }
 
     /*  Handles POST/GET request from: /SearchTags/*
         @param query - String of all queries in format "query1+query2+query3+query4...." 
         Returns a search page listing all segments whose tags that include all the query tags  */
-        public Result getStoriesByTags(String query) throws SQLException, IOException, ClassNotFoundException{
-            myAppController.loadAll();
+        public Result getStoriesByTags(String query){
+            try{
+                myAppController.loadAll();
 
-            String[] queries = query.split("\\+");
+                String[] queries = query.split("\\+");
 
-            ArrayList <Segment> tagged = myAppController.findByTag(queries[0].trim());
-            for(int i = 0; i < queries.length;i++){
-                if(queries[i].substring(0,1)=="#"){
-                    queries[i]=queries[i].substring(1,queries[i].length());
-                }
-            // Get interesection of all searches
-                tagged.retainAll(myAppController.findByTag(queries[i].trim()));
-                for(int p = 0 ; p< tagged.size();p ++){
-                }
-                queries[i] = "\""+queries[i]+"\"";
-            }    
+                ArrayList <Segment> tagged = myAppController.findByTag(queries[0].trim());
+                for(int i = 0; i < queries.length;i++){
+                    if(queries[i].substring(0,1)=="#"){
+                        queries[i]=queries[i].substring(1,queries[i].length());
+                    }
+                // Get interesection of all searches
+                    tagged.retainAll(myAppController.findByTag(queries[i].trim()));
+                    for(int p = 0 ; p< tagged.size();p ++){
+                    }
+                    queries[i] = "\""+queries[i]+"\"";
+                }    
 
-            String searchString = "Search results for tags "+String.join(",",queries);
-            return ok(search.render(searchString,tagged));
+                String searchString = "Search results for tags "+String.join(",",queries);
+                return ok(search.render(searchString,tagged));
+            } catch ( SQLException e | IOException i | ClassNotFoundException c){
+                return internalServerError(views.html.error.render("Something went awfully wrong...please contant the website administrator."));
+            }
         }
 
     /*  Handles POST request from: /SearchTitles/*
         @param query - String of all queries in format "query1+query2+query3+query4...." 
         Returns a search page listing all segments whose title contains all the query titles*/
-        public Result getStoriesByTitles(String query) throws SQLException,IOException,ClassNotFoundException{
-            myAppController.loadAll();
+        public Result getStoriesByTitles(String query){
+            try{
+                myAppController.loadAll();
 
-            String[] queries = query.split("\\+");
+                String[] queries = query.split("\\+");
 
-            ArrayList <Segment> titles = myAppController.findByTitle(queries[0].trim());
+                ArrayList <Segment> titles = myAppController.findByTitle(queries[0].trim());
 
-            for(int i = 0; i < queries.length; i++){
-            // Get intersection of all searches
-                titles.retainAll(myAppController.findByTitle(queries[i].trim()));
-                queries[i] = "\""+queries[i]+"\"";
+                for(int i = 0; i < queries.length; i++){
+                // Get intersection of all searches
+                    titles.retainAll(myAppController.findByTitle(queries[i].trim()));
+                    queries[i] = "\""+queries[i]+"\"";
+                }
+                String searchString = "Search results for titles containing "+String.join(",",queries);
+                return ok(search.render(searchString,titles));
+            } catch ( SQLException e | IOException i | ClassNotFoundException c){
+                return internalServerError(views.html.error.render("Something went awfully wrong...please contant the website administrator."));
             }
-            String searchString = "Search results for titles containing "+String.join(",",queries);
-            return ok(search.render(searchString,titles));
         }
 
 
