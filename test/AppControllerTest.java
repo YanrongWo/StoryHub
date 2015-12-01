f286e229395446a5938065f1c1385fc1bda6ba16
Kellie Ren Lu <krl2130@columbia.edu>
2015-11-30 21:31:05 -0500

    Added test database to AppControllerTest

--- test/AppControllerTest.java
+++ test/AppControllerTest.java
@@ -16,9 +16,43 @@ import play.libs.F;
 import play.libs.F.*;
 import play.twirl.api.Content;
 
+import play.db.Database;
+import play.db.Databases;
+import play.db.evolutions.*;
+import java.sql.Connection;
+
+import org.junit.*;
+import static org.junit.Assert.*;
 import static play.test.Helpers.*;
 import static org.junit.Assert.*;
+import com.google.common.collect.*;
 
 public class AppControllerTest{
-	
+	Database database;
+    Connection connection;
+
+    @Before
+    public void createDatabase() {
+        database = Databases.createFrom(
+            "test",
+            "com.mysql.jdbc.Driver",
+            "jdbc:mysql://test.ctsufn7qqcwv.us-west-2.rds.amazonaws.com:3306",
+            ImmutableMap.of(
+                "user", "javathehutt",
+                "password", "starwars"
+            )
+        );
+        connection = database.getConnection();
+    }
+
+    @After
+    public void shutdownDatabase() {
+        database.shutdown();
+    }
+
+    @Test
+    public void simpleCheck() {
+        int a = 1 + 1;
+        assertEquals(2, a);
+    }
 }
