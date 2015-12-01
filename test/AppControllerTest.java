c4d4289931bf576c550c3a9a9173ecb302b4e517
Kellie Ren Lu <krl2130@columbia.edu>
2015-11-30 23:00:51 -0500

    Added find functions in AppControllerTest'

--- test/AppControllerTest.java
+++ test/AppControllerTest.java
@@ -26,6 +26,10 @@ import static org.junit.Assert.*;
 import static play.test.Helpers.*;
 import static org.junit.Assert.*;
 import com.google.common.collect.*;
+import controllers.*;
+import models.*;
+import java.util.*;
+import java.sql.SQLException;
 
 public class AppControllerTest{
 	Database database;
@@ -36,7 +40,7 @@ public class AppControllerTest{
         database = Databases.createFrom(
             "test",
             "com.mysql.jdbc.Driver",
-            "jdbc:mysql://test.ctsufn7qqcwv.us-west-2.rds.amazonaws.com:3306",
+            "jdbc:mysql://test.ctsufn7qqcwv.us-west-2.rds.amazonaws.com:3306/test",
             ImmutableMap.of(
                 "user", "javathehutt",
                 "password", "starwars"
@@ -54,5 +58,71 @@ public class AppControllerTest{
     public void simpleCheck() {
         int a = 1 + 1;
         assertEquals(2, a);
+        AppController b = new AppController();
+    }
+
+
+    @Test
+    public void findByTag() throws SQLException{
+    	AppController a = new AppController();
+
+    	// Create a small set of stories and insert them into the database
+    	Segment s1 = new Segment("Segment 1", "Test Author", "Some Test Content", new String[]{"tag1", "tag2"});
+    	a.createStory(s1);
+
+    	Segment s2 = new Segment("Segment 2", "Test Author", "Some Test Content 2", new String[]{"Tag2"});
+    	a.createStory(s2);
+
+    	//Test that you can't search for an empty tag
+    	ArrayList<Segment> results = a.findByTag("");
+    	Set<Segment> expected = new HashSet<Segment>();
+    	assertEquals(results,expected);
+
+    	//Test that searching by "tag2" returns correcy query (both segments)
+    	results = a.findByTag("tag2");
+    	Set<Segment> s = new HashSet<Segment>(results);
+    	expected = new HashSet<Segment>();
+    	expected.add(s1);
+    	expected.add(s2);
+    	assertEquals(s,expected);
+    	//Test that searching by "tag1" returns correct query
+    	results = a.findByTag("tag1");
+    	assertEquals(s1,results.get(0));
+
+    	
+    }
+
+
+    @Test
+    public void findByTitle() throws SQLException{
+    	AppController a = new AppController();
+
+    	//Create a smallset of stories and insert them into the database
+    	// Create a small set of stories and insert them into the database
+    	Segment s1 = new Segment("Segment 1", "Test Author", "Some Test Content", new String[]{"tag1", "tag2"});
+    	a.createStory(s1);
+
+    	Segment s2 = new Segment("Segment Yeti 2", "Test Author", "Some Test Content 2", new String[]{"Tag2"});
+    	a.createStory(s2);
+
+    	//Test that you cannot search for an empty title
+    	ArrayList<Segment> results = a.findByTitle("");
+    	Set<Segment> expected = new HashSet<Segment>();
+    	assertEquals(results,expected);
+
+    	//Test that searching by "Segment" returns both segments
+    	results = a.findByTitle("asdfasdf");
+    	assertEquals(2,results.size());
+    	Set<Segment> s = new HashSet<Segment>(results);
+    	expected = new HashSet<Segment>();
+    	expected.add(s1);
+    	expected.add(s2);
+    	//Print out results
+    	System.out.println(results.get(0).getTitle());
+    	assertEquals(s,expected);
+    	//Test that searching  Yeti returns the target segment
+    	results = a.findByTitle("yeti");
+    	assertEquals(results.get(0),s2);
+
     }
 }
